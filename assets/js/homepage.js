d862385ed46073690a19ca3378cdca3ce4b68d88
Tuanny Ruiz <tuannyruiz@gmail.com>
2018-03-02 22:26:13 -0300

    final

---
+++ assets/js/homepage.js
@@ -0,0 +1,14 @@
+// MUDANÇA DE COR DA BARRA DE NAVEGAÇÃO
+
+$(document).ready(function(){       
+    var scroll_pos = 0;
+    $(document).scroll(function() { 
+      scroll_pos = $(this).scrollTop();
+      if(scroll_pos > 250) {
+        $("nav.navbar-fixed-top").css({'background-color': '#710000', 'transition': 'all 1s ease'});
+      } else {
+        $("nav.navbar-fixed-top").css('background-color', 'transparent');
+      }
+    });
+  });
+  
