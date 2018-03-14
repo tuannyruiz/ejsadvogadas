6857aa816e8699b7b17a7ee53852544ee7496e15
Tuanny Ruiz <tuannyruiz@gmail.com>
2018-03-14 19:00:53 -0300

    atualização site

--- themes/ejsadvogadas/footer.php
+++ themes/ejsadvogadas/footer.php
@@ -8,6 +8,11 @@
           <p>Rio de Janeiro - CEP 20040-007</p>
           <p><i class="zmdi zmdi-phone"></i> (21) 2210-5167</p>
         </div>
+        <div class="sociais text-center col-xs-12">
+          <a class="instagram" target="_blank" href="https://www.instagram.com/ejsadvogadas"><i class="zmdi zmdi-instagram"></i></a>
+          <a class="facebook" target="_blank" href="https://m.facebook.com/EJS-Advogadas-2017292118595519/"><i class="zmdi zmdi-facebook"></i></a>
+          <a class="youtube" target="_blank" href="https://www.youtube.com/channel/UC26jf0yb8VoOtm0DxRnYnnw?view_as=subscriber/"><i class="zmdi zmdi-youtube"></i></a>
+        </div>
         <div class="col-md-2 col-md-offset-5 col-sm-4 col-sm-offset-1">
           <a href="<?php echo get_home_url() ?>"><img src="<?= $home; ?>/assets/img/logo.png" alt="Logo da EJS Advogadas"></a>
         </div>
