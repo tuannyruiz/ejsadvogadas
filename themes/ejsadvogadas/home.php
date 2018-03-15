7cd26595b2085d6742c259083b6dd3297a0856e5
Wellington Freitas <wellington.freitas_b@hotmail.com>
2018-03-15 20:46:40 -0300

    media query define background-image no celular

--- themes/ejsadvogadas/home.php
+++ themes/ejsadvogadas/home.php
@@ -4,18 +4,34 @@
  * description: 
  */
 $topFix = true;
-require_once('header.php');  ?>
+require_once('header.php');
+$img_mobile =  wp_get_attachment_image_src( SCF::get( 'img_mobile' ));
+?>
 
 <style>
     main.pagina-inicial {
-  background: linear-gradient( rgba(113, 0, 0, 0.5),
-   rgba(113, 0, 0, 0.5) ),
-    url('<?php echo fullThumbnailUrl($post->id ) ?>')
-     no-repeat center;  
-     background-size: cover;  
-  height: 1000px;
-  min-height: 100vh;
+        background: linear-gradient( rgba(113, 0, 0, 0.5),
+        rgba(113, 0, 0, 0.5) ),
+         url('<?php echo fullThumbnailUrl($post->id ) ?>')
+        no-repeat center;  
+        background-size: cover;  
+        height: 100%;
+        min-height: 100vh;
     }
+
+    @media only screen and (max-width: 767px) { 
+
+        main.pagina-inicial {
+            background: linear-gradient( rgba(113, 0, 0, 0.5),
+            rgba(113, 0, 0, 0.5) ),
+            url('<?php echo $img_mobile[0] ?>')
+            no-repeat center;  
+            background-size: cover;  
+            height: 1000px;
+            min-height: 100vh;
+            }
+    }
+
 </style>
 
 <main class="pagina-inicial">
