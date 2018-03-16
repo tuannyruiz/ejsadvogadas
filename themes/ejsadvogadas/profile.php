8f078beaf32c78c67d469a44e8ab6a2627d1ae00
Wellington Freitas <wellington.freitas_b@hotmail.com>
2018-03-15 21:51:07 -0300

    incluido botoes profiles

--- themes/ejsadvogadas/profile.php
+++ themes/ejsadvogadas/profile.php
@@ -24,6 +24,13 @@ require_once('header.php');  ?>
         <?php if( SCF::get( 'curriculo_url' ) ) { ?>
           <a href="<?php echo SCF::get( 'curriculo_url' ) ?>" class="btn-adv curriculo" target="blank">Currículo</a>
         <?php } ?>
+        <?php  
+          $array_menu = wp_get_nav_menu_items( 'menu_profiles' );
+          foreach ($array_menu as $m) {
+            $pg_id = get_post_meta( $m->ID, '_menu_item_object_id', true );
+            if (empty($m->menu_item_parent) && $post->ID != $pg_id ) { ?>
+                <a href="<?php echo $m->url; ?>" class="btn-adv"><?php echo $m->title; ?></a>
+          <?php }} ?>
       </div>
     </div>
   </main>
