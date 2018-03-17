9f400161b531d253ac56a33a503d320f72e53a7e
Wellington Freitas <wellington.freitas_b@hotmail.com>
2018-03-17 12:17:55 -0300

    ajustado script que define o tamanho dos cards para funcionar no firefox

--- themes/ejsadvogadas/assets/js/cardlist.js
+++ themes/ejsadvogadas/assets/js/cardlist.js
@@ -1,3 +1,26 @@
+
+function resizeCards() {
+  var cards =document.querySelectorAll(".card-custom .atuacao-card");
+  if(window.innerWidth > 800) {
+
+  var maxSize = 0;
+  var objs = [];
+  for(var i=0, j=1; i<cards.length; i++, j++) {
+    var el = cards[i];
+    
+    if(el.offsetHeight > maxSize) {
+      maxSize = el.offsetHeight;
+    }
+  }
+  for(var r = 0; r < cards.length; r++) {
+        cards[r].style.height = maxSize + "px";
+        console.log(maxSize);
+      }
+  }
+}
+
+resizeCards();
+
 $(document).ready(function() {
 	$('.card-container').click(function() {
     var modal = $(this).parent().find('.modal');
@@ -5,19 +28,3 @@ $(document).ready(function() {
 	});
 });
 
-var cards =document.querySelectorAll(".card-custom .atuacao-card");
-if(window.innerWidth > 800) {
-
-var maxSize = 0;
-var objs = [];
-for(var i=0, j=1; i<cards.length; i++, j++) {
-  var el = cards[i];
-  
-  if(el.offsetHeight > maxSize) {
-    maxSize = el.offsetHeight;
-  }
-}
-for(var r = 0; r < objs.length; r++) {
-      objs[r].style.height = maxSize + "px";
-    }
-}
